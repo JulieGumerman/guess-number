1f5b585037566b0cea9fbc43021ac063a6db3754
bodhibrdr <julie.gumerman@gmail.com>
2019-11-04 13:47:03 -0700

    game-over functionality

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,13 +1,15 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { View, Text, StyleSheet, Button, Alert} from "react-native";
 
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
 
 const generateRandomBetween = (min, max, exclude) => {
+
     min = Math.ceil(min);
     max = Math.floor(max);
     const rndNum = Math.floor(Math.random() * (max-min) + min);
+
 
     if (rndNum === exclude ) {
         return generateRandomBetween(min, max, exclude);
@@ -19,10 +21,17 @@ const generateRandomBetween = (min, max, exclude) => {
 const GameScreen = props => {
 
     const [ currentGuess, setCurrentGuess ] = useState(generateRandomBetween(1, 100, props.userChoice))
-
+    const [rounds, setRounds] = useState(0)
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
+
+    const { userChoice, onGameOver } = props;
  
+    useEffect(() => {
+        if (currentGuess === userChoice) {
+            onGameOver(rounds);
+        }
+    }, [ currentGuess, userChoice, onGameOver])
     
     const nextGuessHandler = direction => {
         if ((direction === "lower"  && currentGuess < props.userChoice) || (direction === "greater" && currentGuess > props.userChoice)) {
@@ -37,6 +46,7 @@ const GameScreen = props => {
 
         const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
         setCurrentGuess(nextNumber);
+        setRounds(curRounds => curRounds + 1);
     }
 
     return (
